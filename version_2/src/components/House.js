e21e327edaed309be1278e2118ae4ebb0524d5ce
kevinjin813 <55816605+kevinjin813@users.noreply.github.com>
2020-06-13 18:40:13 -0400

    add login into house page

--- version_2/src/components/House.js
+++ version_2/src/components/House.js
@@ -3,9 +3,20 @@ import HouseRentForm from "../container/HouseRentForm";
 import React, {useState} from 'react';
 const House = () => {
       return (
-        <div className='house'>
-          <h1>Houses For Rent</h1>
+        <div className='house'> 
+          <h1>login</h1> 
+          <div id="login">
+            <form className="login form">
+            <label for="name">User Name</label>
+            <input type="name" name="username" value=""/>
+            <label for="password">Password</label>
+            <input type="password" name="password" value=""/>
+            <input class="button" type="submit" value="submit"/>
+          </form> 
+          </div>   
+          <h2>Houses For Rent</h2>
           <HouseRentForm data={[{}]}/>
+          
         </div>
       )
 }
---
+++ version_2/src/container/LoginForm.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import useStickyState from '../useStickyState';
+import styled from 'styled-components';
+import '../generalStyle.css';
+
+const  LoginForm = () => {
+  return 
+  {
+    
+  };
+}
+
+
+export default LoginForm; 
