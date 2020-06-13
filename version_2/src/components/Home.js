ed1ac6cb6eeabffc63dbd94f17ddb2b0c33abfdc
Kailin Liu <kailinliu@brandeis.edu>
2020-06-13 19:30:54 -0400

    delete some redundancy

--- version_2/src/components/Home.js
+++ version_2/src/components/Home.js
@@ -29,30 +29,25 @@ margin-left: 420px;
 `;
 
 const Home = () => {
-
-      return (
-        <div className='container-fluid'>
-        <div className = 'first'>
+    return (
+      <div className='first'>
         <TitleWrapper>
           <h1>Waltham Community Forum</h1>
         </TitleWrapper>
         <SubtitleWrapper>
-        <h2>Welcome!</h2>
+          <h2>Welcome!</h2>
         </SubtitleWrapper>
-        </div>
-        <div className = 'second'>
         <TextWrapper>
-        <p className = 'space'> Welcome to the Waltham Community Forum. With the COVID-19 prevailing, there are a lot of arising
+          <p className = 'space'> Welcome to the Waltham Community Forum. With the COVID-19 prevailing, there are a lot of arising
             concerns over the accessibility of food, daily supplies, and housing. Also, it will be important
             to get information about the number of peopled infected nearby.</p>
-        <p className = 'space'> We build this forum so that people in Waltham can get informed about the operating status of restaurant, shops,
+          <p className = 'space'> We build this forum so that people in Waltham can get informed about the operating status of restaurant, shops,
             houses for rent, and updates on COVID-19.</p>
-        <p className = 'space'> Since this is a forum, there are places where people can post info they know and share with each other.</p>
-        <p className = 'space'> Stay Safe!</p>
+          <p className = 'space'> Since this is a forum, there are places where people can post info they know and share with each other.</p>
+          <p className = 'space'> Stay Safe!</p>
         </TextWrapper>
-        </div>
-        </div>
-      )
+      </div>
+    )
 }
 
 export default Home;
--- version_2/src/components/House.js
+++ version_2/src/components/House.js
@@ -3,8 +3,8 @@ import HouseRentForm from "../container/HouseRentForm";
 import React, {useState} from 'react';
 const House = () => {
       return (
-        <div className='house'> 
-          <h1>login</h1> 
+        <div className='house'>
+          <h1>login</h1>
           <div id="login">
             <form className="login form">
             <label for="name">User Name</label>
@@ -12,11 +12,11 @@ const House = () => {
             <label for="password">Password</label>
             <input type="password" name="password" value=""/>
             <input class="button" type="submit" value="submit"/>
-          </form> 
-          </div>   
+            </form> 
+          </div>
           <h2>Houses For Rent</h2>
           <HouseRentForm data={[{}]}/>
-          
+
         </div>
       )
 }
