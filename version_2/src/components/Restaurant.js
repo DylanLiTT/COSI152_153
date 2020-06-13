845564c8ba11acdaeb4a7cdbb97d9b99f12e0cca
Kailin Liu <kailinliu@brandeis.edu>
2020-06-13 17:47:21 -0400

    change

--- version_2/src/components/Restaurant.js
+++ version_2/src/components/Restaurant.js
@@ -3,24 +3,18 @@ import BgColor from "./BgColor";
 import restaurantdata from "../assets/restaurantdata.js";
 import RestaurantList from "../container/RestaurantList";
 const Restaurant = () => {
-
-
     const [bgColor,setBgColor]= useState("white")
-
-
-
     const divStyle={backgroundColor: bgColor}
-
     return (
-
         <div style={divStyle}>
-          <BgColor bgColor={bgColor} setBgColor={setBgColor} />
           <h1>Restaurants Opening</h1>
-          <p> This page shows the restaurant that are currently Opening.
-          </p>
+          <p> This page shows the restaurant that are currently Opening.</p>
+          <BgColor bgColor={bgColor} setBgColor={setBgColor} />
           <RestaurantList restaurantdata={restaurantdata} />
         </div>
-      )
+    )
+
+
 }
 
 export default Restaurant;
