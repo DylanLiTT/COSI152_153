150fb0480a012ff06bf0da759721ddba6afaac87
kevinjin813 <55816605+kevinjin813@users.noreply.github.com>
2020-06-17 22:11:13 -0400

    fix localhost problem

--- web_version_2/app.js
+++ web_version_2/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-
+const User = require('./models/User');
 const mongoose = require( 'mongoose' );
 mongoose.connect( 'mongodb://localhost/authDemo');
 
@@ -34,7 +34,7 @@ const express = require("express"),
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+//app.use(cors());
 app.use(layouts);
 
 app.use(logger('dev'));
@@ -47,59 +47,16 @@ app.use(authRouter)
 app.use(loggingRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/db',dbRouter);
-app.use('/dbdemo',
-    (req,res) => res.render('dbdemo'))
-
-app.use('/db',dbRouter);
-// app.use('/todo',toDoRouter);
 
 
-
-
-const User = require('./models/User');
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-app.set("view engine", "ejs");
-app.set("port", process.env.PORT || 2000);
-app.use(
-  express.urlencoded({
-    extended: false
-  })
-);
-app.use(express.json());
-app.use(layouts);
-app.use(express.static("public"));
-
-
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.get("/houseForRent", homeController.showHouseForRent);
-app.get("restaurant", homeController.showRestaurantOpenNow);
+
+app.get("/restaurant", homeController.showRestaurantOpenNow);
 app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedSignUpForm);
 
-app.use(authRouter);
-app.use(loggingRouter);
-app.use('/', indexRouter);
+
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
